953a6b027f72b804d6f593682ad66b5ea96389de
LegendALV3 <legendalv@duck.com>
2024-07-19 09:35:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,9 +24,9 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.
 global.caption = process.env.CAPTION || global.caption || "LEGENDARY" 
 
 
-global.devs = "919403621818" // Developer Contact
+global.devs = "2349156137725" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "919403621818";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "919403621818";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2349156137725";
 
 
 
